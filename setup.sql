39acf3d9e93fd6c222cbff0119809a124eb38f16
ganggreen46 <110796422+ganggreen46@users.noreply.github.com>
2025-07-02 14:46:25 -0700

    added and updated semantic view

--- setup.sql
+++ setup.sql
@@ -13,4 +13,14 @@ CREATE or replace GIT REPOSITORY git_repo_semantic_view
 api_integration = git_api_integration
 origin = 'https://github.com/galaga2000/semantic-view';
 
+
+-- Make sure we get the latest files
+ALTER GIT REPOSITORY git_repo FETCH;
 show api integrations;
+
+current_account();
+current_user();
+current_database();
+current_schema();
+
+select '-a ' || current_account() || ' -u ' || current_user() || ' -d ' || current_database() || ' -s ' || current_schema();
